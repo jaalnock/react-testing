c0461c7365835414563513c4cce9ad0c80827feb
Sahil Katkamwar <sahilkatkamwar24@gmail.com>
2025-01-27 02:11:26 +0530

    Added test to check the toggle from child component

--- src/__tests__/child.test.jsx
+++ src/__tests__/child.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Child from "../components/child";
 
@@ -7,4 +7,16 @@ test("should check props value", () => {
   const getElement = getByTestId("child-count-value");
 
   expect(getElement.textContent).toBe("786");
+});
+
+test("should toggle the theme", () => {
+  const { getByTestId } = render(<Child count={500} />);
+  const getThemeElement = getByTestId("toggle-text");
+  const getButtonElement = getByTestId("toggle-button");
+
+  expect(getThemeElement.textContent).toBe("dark");
+
+  fireEvent.click(getButtonElement);
+
+  expect(getThemeElement.textContent).toBe("light");
 });
--- src/components/child.jsx
+++ src/components/child.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Child = ({ count }) => {
+  const [theme, setTheme] = useState(true);
+
   return (
     <div>
       <p data-testid="child-count-value">{count}</p>
+      <button onClick={() => setTheme(!theme)} data-testid="toggle-button">
+        Toggle
+      </button>
+      <h2 data-testid='toggle-text'>{theme ? "dark" : "light"}</h2>
     </div>
   );
 };
