eefe230fb85a97f966432faf88c8436213a51081
Sahil Katkamwar <sahilkatkamwar24@gmail.com>
2025-01-27 04:09:41 +0530

    Test for Toggling Theme Between Light and Dark on Button Click
    This test checks if the theme toggles between light and dark when the button is clicked. Initially, it expects the theme to be light. After the first click, it verifies the theme changes to dark, and after the second click, it ensures the theme switches back to light. The test ensures that the button correctly toggles the theme state and the UI updates accordingly.

--- src/__tests__/context-test/context.test.jsx
+++ src/__tests__/context-test/context.test.jsx
@@ -3,8 +3,21 @@
 
 import React from "react";
 import "@testing-library/jest-dom";
-import { ThemeProvider } from "../../context";
-import { render } from "@testing-library/react";
+import { ThemeProvider, useTheme } from "../../context";
+import { fireEvent, render } from "@testing-library/react";
+import ThemeRender from "../../components/themeRender";
+
+const ToggleConsumer = () => {
+  const { toggleTheme } = useTheme();
+  return (
+    <div>
+      <button role="button" onClick={toggleTheme}>
+        Toggle Theme
+      </button>
+      <ThemeRender />
+    </div>
+  );
+};
 
 describe("should test context theme provider", () => {
   // content which we are passing to our context provider must be there
@@ -16,5 +29,27 @@ describe("should test context theme provider", () => {
     );
 
     expect(getByText("This is a child component")).toBeInTheDocument();
+  });
+
+  it("toggles between light and dark value on click of button", () => {
+    const { getByText, getByRole } = render(
+      <ThemeProvider>
+        <ToggleConsumer />
+      </ThemeProvider>
+    );
+
+    const getToggleButtonByRole = getByRole("button", {
+      name: /toggle theme/i,
+    });
+
+    expect(getByText("Current theme is: light")).toBeInTheDocument();
+
+    fireEvent.click(getToggleButtonByRole);
+
+    expect(getByText("Current theme is: dark")).toBeInTheDocument();
+
+    fireEvent.click(getToggleButtonByRole);
+
+    expect(getByText("Current theme is: light")).toBeInTheDocument();
   });
 });
