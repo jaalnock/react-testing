db4aa8132c5e82df0d8bb4808c38bcb2f2beb41a
Sahil Katkamwar <sahilkatkamwar24@gmail.com>
2025-01-26 20:09:55 +0530

    Created Test for Counter Click
    Firstly getting the element by text
    so it'll be Count is: 0
    After getting text, getting button element
    so that we can fire an event on it of click
    After event check if counter value has increased by 1
    
    There is also one another method using 'getByRole'

---
+++ src/__tests__/counter.test.jsx
@@ -0,0 +1,38 @@
+import { fireEvent, render } from "@testing-library/react";
+import HelloWorld from "../components/helloWorld";
+import "@testing-library/jest-dom";
+import Counter from "../components/counter";
+
+test("should test counter logic", () => {
+  // Firstly getting the element by text
+  // so it'll be Count is: 0
+  // After getting text, getting button element
+  // so that we can fire an event on it of click
+  // After event check if counter value has increased by 1
+
+  const { getByText } = render(<Counter />);
+  const getCounterText = getByText("Count is: 0");
+  const getButtonElementByText = getByText("Click");
+
+  // clicking the button having text click
+  fireEvent.click(getButtonElementByText);
+
+  expect(getCounterText.textContent).toBe("Count is: 1");
+
+  fireEvent.click(getButtonElementByText);
+
+  expect(getCounterText.textContent).toBe("Count is: 2");
+});
+
+
+// Another way to test the same using "getByRole"
+test('increases counter on button click', () => {
+  const { getByText, getByRole } = render(<Counter />);
+
+  const counterText = getByText(/Count is:/);
+  const button = getByRole('button', { name: /Click/ });
+
+  fireEvent.click(button);
+
+  expect(counterText).toHaveTextContent('Count is: 1');
+});
